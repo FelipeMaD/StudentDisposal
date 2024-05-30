fe5fb3a14f8453b5d308be21f8f271a700825768
FelipeMaD <felipemarinhodr@hotmail.com>
2024-05-30 15:26:22 -0300

    update views, urls e templates
    adição do crud completo de criação de estudantes para o cadastro

--- studentsPost/urls.py
+++ studentsPost/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
-
+from .views import StudentsCreateView, StudentsDetailView, StudentsUpdateView, StudentsDeleteView, StudentsListView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-]
+    path('', StudentsListView.as_view(), name='post_list'),
+    path('create/', StudentsCreateView.as_view(), name='post_create'), 
+    path('<int:pk>/', StudentsDetailView.as_view(), name='post_detail'), 
+    path('<int:pk>/update/', StudentsUpdateView.as_view(), name='post_update'), 
+    path('<int:pk>/delete/', StudentsDeleteView.as_view(), name='post_delete'),  
+] 
--- studentsPost/views.py
+++ studentsPost/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from .models import StudentsPost
+from django.views.generic import ListView, DeleteView, DetailView, UpdateView, CreateView
+from django.urls import reverse_lazy
 
+
+class StudentsCreateView(CreateView):
+    model = StudentsPost
+    fields = ['matricula', 'nome', 'sobrenome', 'serie', 'descricao'] 
+    template_name = 'pages/post_create.html'
+    success_url = '/students/'
+
+class StudentsDetailView(DetailView):
+    model = StudentsPost
+    template_name = 'pages/post_detail.html'
+
+class StudentsUpdateView(UpdateView):
+    model = StudentsPost
+    fields = ['matricula', 'nome', 'sobrenome', 'serie', 'descricao'] 
+    template_name = 'pages/post_update.html'
+    success_url = '/students/' 
+
+class StudentsDeleteView(DeleteView):
+    model = StudentsPost
+    template_name = 'pages/post_delete.html'
+    success_url = reverse_lazy('post_list') 
+
+class StudentsListView(ListView):
+    model =  StudentsPost
+    template_name = 'pages/post_list.html'
+    context_object_name = 'posts'
+
+
+'''
 def post_list(request):
     posts = StudentsPost.objects.all()
     return render(request, 'pages/post_list.html', {'posts': posts})
@@ -9,3 +40,4 @@ def post_detail(request, pk):
     return render(request, 'pages/post_detail.html', {'post': post})
 
 # Create your views here.
+'''
